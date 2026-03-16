564b5b1fd423f17a6cbf38259b58f06b9b87c946
kartofan33100-spec <kartofan33100@gmail.com>
2026-03-17 00:36:38 +0500

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = ({ user }) => {
     <section className="hero container">
       <div className="hero_content">
         <div className="hero_text">
-          <h1>Отслеживай цели, дедлайны и прогресс в одном месте</h1>
+          <h1>Отслеживай цель, дедлайны и прогресс в одном месте</h1>
           <p>
             Goal Tracker помогает создавать личные цели, отмечать процент выполнения
           </p>
